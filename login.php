f2ecdb393778facabdf9c7f9167010f1d35a3e7e
Alisha <sjl0504sjl@gmail.com>
2025-05-11 16:43:00 +1000

    login test

--- login.php
+++ login.php
@@ -9,3 +9,17 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') exit(0);
+
+setcookie("strata_session", "active", [
+  'httponly' => true,
+  'secure' => true,
+  'samesite' => 'Strict',
+  'path' => '/',
+]);
+
+setcookie("user_id", $user["id"], [
+  'httponly' => true,
+  'secure' => true,
+  'samesite' => 'Strict',
+  'path' => '/',
+]);
